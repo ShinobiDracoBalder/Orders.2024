20507e1a34d230957ff39f55fa9d1f603f935daa
dracomaster <43005960+ShinobiDracoBalder@users.noreply.github.com>
2024-02-25 12:25:11 -0600

    se quito el watherforecast controller como la clase

---
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Orders.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+    }
+}
